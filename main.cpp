e96c3ccb62d6a192bdf7c6d5984ac574f06e6683
sebasflorez55 <sebasflorez55@gmail.com>
2020-10-22 09:09:22 -0500

    tercer coomit

--- main.cpp
+++ main.cpp
@@ -10,5 +10,12 @@ int main()
     for  (int var = 0; var < TAM; var++){
         cout << a[var]<<endl;
     }
+    cout << "Imprimir los numeros de la cadena original"<< endl;
+     for (int var = 0;var < TAM;var++){
+         if (a [var] >= '0' or a[var] <= '9'){
+             cout << a[var] << endl;
+         }
+         cout << ""<< endl;
+     }
     return 0;
 }
