dbb1a590516b9f148256e64264575c4643f87051
sebasflorez55 <sebasflorez55@gmail.com>
2020-10-22 08:57:31 -0500

    se crea cadena de caracteres
    Signed-off-by: sebasflorez55 <sebasflorez55@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#define TAM (int)10 //variables globales y macros
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char a[TAM] = {"AB11"};
+
+    for  (int var = 0; var < TAM; var++){
+        cout << a[var]<<endl;
+    }
     return 0;
 }
